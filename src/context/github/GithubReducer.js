4978cbba009f81f92c1aeb2127009bc36c82fb17
Gorbataras <47342116+Gorbataras@users.noreply.github.com>
2022-03-14 09:26:55 -0700

    Using Reduce instead of State

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,17 @@
+const githubReducer = (state, action) => {
+    switch (action.type) {
+        case 'GET_USERS':
+            return {
+                ...state,
+                users: action.payload,
+                loading: false,
+            }
+            break;
+    
+        default:
+            return state;
+            break;
+    }
+}
+
+export default githubReducer
